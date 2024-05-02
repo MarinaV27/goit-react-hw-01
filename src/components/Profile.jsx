888b79eb3322d579a39440b7e2c47b493b3042c6
MarinaV27 <Vasilishinamari@gmail.com>
2024-05-02 23:29:50 +0300

    exercise 1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,40 +1,29 @@
-export default function Profile(){
-    const userData = {
-        username: "Jacques Gluke",
-        tag: "jgluke",
-        location: "Ocho Rios, Jamaica",
-        avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-        stats: {
-          followers: 5603,
-          views: 4827,
-          likes: 1308
-        }
-      };
-      
+export default function Profile({image, name, tag, location, stats}){
+     
     return (
         <div>
  <div>
   <img 
-   src={userData.avatar}
+   src={image}
    alt="User avatar"
   />
-  <p>{userData.username}</p>
-  <p>@{userData.tag}</p>
-  <p>{userData.location}</p>
+  <p>{name}</p>
+  <p>@{tag}</p>
+  <p>{location}</p>
  </div>
 
  <ul>
   <li>
    <span>Followers</span>
-   <span>{userData.stats.followers}</span>
+   <span>{stats.followers}</span>
   </li>
   <li>
    <span>Views</span>
-   <span>{userData.stats.views}</span>
+   <span>{stats.views}</span>
   </li>
   <li>
    <span>Likes</span>
-   <span>{userData.stats.likes}</span>
+   <span>{stats.likes}</span>
   </li>
  </ul>
 </div>
